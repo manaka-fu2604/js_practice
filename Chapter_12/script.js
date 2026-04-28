7cf7f7ea3dc8a7ecff239ba467a2b779ef2d2b65
Manaka Fumiyori <manaka-fu2604@ibjapan.jp>
2026-04-28 16:35:54 +0900

    add Chapter_12

---
+++ Chapter_12/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const p = document.getElementById('output'); // 出力用のp要素
+
+// ［表示］ボタンを押したら…
+document.querySelector('button').addEventListener('click', () => {
+  // 今回のコードはここに書いていきます
+});
