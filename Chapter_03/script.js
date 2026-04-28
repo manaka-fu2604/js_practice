35545745720580308a73580a3ba4fdf92ecbe97c
Manaka Fumiyori <manaka-fu2604@ibjapan.jp>
2026-04-28 09:33:34 +0900

    add files

--- Chapter_03/script.js
+++ Chapter_03/script.js
@@ -1,4 +1,16 @@
 'use strict';
 
-const bmi = 27;
-console.log(`あなたのBMIは${bmi}です`);
+// 入力ダイアログから値を指定する
+const bmi = window.prompt('BMIの値を入力してください');
+console.log(`あなたのBMIは${bmi}：`);
+
+if (bmi > 25) {
+  console.log('肥満です');  // 25より大きいときに表示
+} else if (bmi < 18.5) {
+  console.log('低体重です');  // 18.5より小さいときに表示
+} else {
+  console.log('適正体重です');  // 25以下のときに表示
+}
+// 略
+const p_element = document.querySelector('p');
+console.log(p_element);
