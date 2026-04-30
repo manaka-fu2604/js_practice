353c574de0fa6e1d5cbb1f2e53a9372a975041e9
Manaka Fumiyori <manaka-fu2604@ibjapan.jp>
2026-04-30 14:42:05 +0900

    fix files

--- Chapter_01/script.js
+++ Chapter_01/script.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-document.querySelector('p').textContent = 'Hello World!!';
-// window.alert('Hello World!!');
-console.log('Hello World!!');
-=======
 document.querySelector("p").textContent = "こんにちは、世界！！";
 // window.alert("Hello World!!");
 console.log("Hello World!!");
->>>>>>> 01
