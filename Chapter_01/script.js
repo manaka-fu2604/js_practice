9e7387eca5877777288d1ad76e0df298ea8aea5a
Manaka Fumiyori <manaka-fu2604@ibjapan.jp>
2026-05-15 11:21:43 +0900

    復習

--- Chapter_01/script.js
+++ Chapter_01/script.js
@@ -1,3 +1,4 @@
 document.querySelector("p").textContent = "こんにちは、世界！！";
-// window.alert("Hello World!!");
+window.alert("Hello World!!\nこんにちは、世界！！");
+window.alert("こんにちは、世界！！");
 console.log("Hello World!!");
